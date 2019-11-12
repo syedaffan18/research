68f9c9c1b8587ca909ad8ad80fda7f38a8dffaac
syedaffan18 <syedaffanaslam@gmail.com>
2019-11-12 21:55:47 +0500

    devops

--- cronjob/Build/misc/table.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE stringlog (
-    Stringid int NOT NULL AUTO_INCREMENT,
-    StringVal varchar(255) NOT NULL,
-    PRIMARY KEY (Stringid)
-);
-
-INSERT INTO stringlog (StringVal) VALUES ('Hello World!')
